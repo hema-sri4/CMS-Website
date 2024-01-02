9e6061986921bca8cc7d1f05bac9c079d15ee460
Hemasri Cheekati <85050196+hema-sri4@users.noreply.github.com>
2024-01-02 11:24:59 +0530

    Add files via upload

---
+++ Admin/dbconnect.php
@@ -0,0 +1,14 @@
+<?php 
+$host="localhost";
+$username="root";
+$password="";
+$dbname="college";
+$link = mysqli_connect($host, $username, $password)
+
+        or die("Could not connect");
+
+$db = mysqli_select_db($link, $dbname)
+		or die("Could not select database");
+?>
+
+
